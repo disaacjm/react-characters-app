a7d72bd60f1557154cd41f21d8fbd86008826d5c
disaacjm <disaacjm@gmail.com>
2023-06-22 16:04:40 +0100

    basic routing + function renderListOfCharacters

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect } from "react";
+import axios from "axios";
+import { useState } from "react";
+import { Route, Routes, NavLink } from "react-router-dom";
 
 function App() {
+  const baseURL = "https://ih-crud-api.herokuapp.com";
+
+  const [charactersArr, setCharactersArr] = useState(null);
+
+  useEffect(() => {
+    axios
+      .get(`${baseURL}/characters`)
+      .then((response) => {
+        setCharactersArr(response.data);
+      })
+      .catch((e) => console.log(e));
+  }, []);
+
+  const renderListOfCharacters = () => {
+    if (charactersArr === null) {
+      return <p>loading....</p>;
+    } else {
+      return charactersArr.map((characterObj) => {
+        return (
+          <div key={characterObj.id} className="character box">
+            Name: {characterObj.name} <br />
+            Weapon: {characterObj.weapon}
+          </div>
+        );
+      });
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>React Characters App</h1>
+
+      <nav>
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/about">About</NavLink>
+        <NavLink to="/contact">Contact</NavLink>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={renderListOfCharacters()} />
+        <Route path="/about" element={<p>Display About page</p>} />
+        <Route path="/contact" element={<p>Display Contact page</p>} />
+      </Routes>
     </div>
   );
 }
